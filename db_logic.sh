405476e453cb22653a95c804966dcbcb17b7c0d1
MariamAsall <mariamasal075@gmail.com>
2026-03-26 00:42:56 +0200

    Create Database Function created  with validation in db_layer

---
+++ db_logic.sh
@@ -0,0 +1,22 @@
+:'
+THIS FILE CONTAINS THE LOGIC FOR THE DATABASE CREATION AND SEEDING
+1- CREATE THE DATABASE
+2- CONNECT TO THE DATABASE
+3- LIST THE TABLES IN THE DATABASE
+4- DROP THE TABLES IN THE DATABASE
+'
+# 1- CREATE THE DATABASE
+create_db(){
+    read -p "Enter the name of the database to create: " db_name
+    if [[ -z $db_name ]] then
+        echo "Database name is empty Please enter a valid name"
+    elif [[ -d $db_name ]]; then 
+        echo "Database already exists Please choose a different name"
+    elif [[ $db_name =~ [^a-zA-Z0-9_] ]] then
+        echo "Database name should only contain letters, numbers, and underscores"
+    else
+        mkdir "$db_name"
+        echo "Database $db_name created successfully"
+    fi
+    
+}
